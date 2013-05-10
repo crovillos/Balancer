86387609bbbcff2a4938b6e26f7fb1b69b709c6b
Dian Hartono <dhartono@uw.edu>
2013-05-09 21:55:30 -0700

    invite subclasses

---
+++ Balancer/Model/ActivityInvite.h
@@ -0,0 +1,13 @@
+//
+//  ActivityInvite.h
+//  Balancer
+//
+//  Created by Dian Hartono on 5/9/13.
+//  Copyright (c) 2013 Dian Hartono, Grace Jang, Chris Rovillos, Catriona Scott, Brian Yin. All rights reserved.
+//
+
+#import "Invite.h"
+
+@interface ActivityInvite : Invite
+
+@end
---
+++ Balancer/Model/ActivityInvite.m
@@ -0,0 +1,13 @@
+//
+//  ActivityInvite.m
+//  Balancer
+//
+//  Created by Dian Hartono on 5/9/13.
+//  Copyright (c) 2013 Dian Hartono, Grace Jang, Chris Rovillos, Catriona Scott, Brian Yin. All rights reserved.
+//
+
+#import "ActivityInvite.h"
+
+@implementation ActivityInvite
+
+@end
