d4f2f296a7977af20a63bf1a5a18de6ef65d9555
Chris Rovillos <crovillos@icloud.com>
2013-05-09 21:06:40 -0700

    Added new tabs and accompanying controller subclasses.

--- Balancer/ProgressNavigationController.h
+++ Balancer/ProgressNavigationController.h
@@ -1,13 +1,13 @@
 //
-//  FirstViewController.h
+//  ProgressNavigationController.h
 //  Balancer
 //
-//  Created by Chris Rovillos on 5/3/13.
+//  Created by Chris Rovillos on 5/9/13.
 //  Copyright (c) 2013 Dian Hartono, Grace Jang, Chris Rovillos, Catriona Scott, Brian Yin. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface FirstViewController : UIViewController
+@interface ProgressNavigationController : UINavigationController
 
 @end
---
+++ Balancer/ProgressNavigationController.m
@@ -0,0 +1,38 @@
+//
+//  ProgressNavigationController.m
+//  Balancer
+//
+//  Created by Chris Rovillos on 5/9/13.
+//  Copyright (c) 2013 Dian Hartono, Grace Jang, Chris Rovillos, Catriona Scott, Brian Yin. All rights reserved.
+//
+
+#import "ProgressNavigationController.h"
+
+@interface ProgressNavigationController ()
+
+@end
+
+@implementation ProgressNavigationController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
