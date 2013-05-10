244a14a41146f2893b0ff8003b9e2a8be3a591dc
Dian Hartono <dhartono@uw.edu>
2013-05-09 21:53:22 -0700

    Invite

---
+++ Balancer/Model/Invite.h
@@ -0,0 +1,15 @@
+//
+//  Invite.h
+//  Balancer
+//
+//  Created by Dian Hartono on 5/9/13.
+//  Copyright (c) 2013 Dian Hartono, Grace Jang, Chris Rovillos, Catriona Scott, Brian Yin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Invite : NSObject
+@property  (nonatomic, strong) NSString* sender;
+@property (nonatomic, strong) NSDate* sentDate;
+
+@end
