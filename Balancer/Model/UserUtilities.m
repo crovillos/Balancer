f59843476dd52f1f266c570942c622ea7d917387
Dian Hartono <dhartono@uw.edu>
2013-05-09 22:17:51 -0700

    user utility

---
+++ Balancer/Model/UserUtilities.m
@@ -0,0 +1,13 @@
+//
+//  UserUtilities.m
+//  Balancer
+//
+//  Created by Dian Hartono on 5/9/13.
+//  Copyright (c) 2013 Dian Hartono, Grace Jang, Chris Rovillos, Catriona Scott, Brian Yin. All rights reserved.
+//
+
+#import "UserUtilities.h"
+
+@implementation UserUtilities
+
+@end
