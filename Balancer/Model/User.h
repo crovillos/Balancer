cb0e50b62f9e6955207fadb761dc1d6d1c25f832
Dian Hartono <dhartono@uw.edu>
2013-05-09 22:05:38 -0700

    users

--- Balancer/Model/User.h
+++ Balancer/Model/User.h
@@ -2,15 +2,14 @@
 //  User.h
 //  Balancer
 //
-//  Created by Catriona Scott on 5/9/13.
+//  Created by Dian Hartono on 5/9/13.
 //  Copyright (c) 2013 Dian Hartono, Grace Jang, Chris Rovillos, Catriona Scott, Brian Yin. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 @interface User : NSObject
-
-@property (nonatomic) NSInteger fbid;
-// UIImage for fb photo
+@property  (nonatomic) NSInteger fbid; //need to look at fb sdk
+//fb_photo need to figure out what type
 
 @end
--- Balancer/Model/User.m
+++ Balancer/Model/User.m
@@ -2,14 +2,12 @@
 //  User.m
 //  Balancer
 //
-//  Created by Catriona Scott on 5/9/13.
+//  Created by Dian Hartono on 5/9/13.
 //  Copyright (c) 2013 Dian Hartono, Grace Jang, Chris Rovillos, Catriona Scott, Brian Yin. All rights reserved.
 //
 
 #import "User.h"
 
 @implementation User
-
-@
 
 @end
