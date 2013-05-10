497c747ee3dd2ba47ddcee41bee34a1aa3bb054e
Dian Hartono <dhartono@uw.edu>
2013-05-09 21:49:47 -0700

    activity ready

---
+++ Balancer/Model/Activity.h
@@ -0,0 +1,22 @@
+//
+//  Activity.h
+//  Balancer
+//
+//  Created by Dian Hartono on 5/9/13.
+//  Copyright (c) 2013 Dian Hartono, Grace Jang, Chris Rovillos, Catriona Scott, Brian Yin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Activity : NSObject
+
+@property (nonatomic) NSInteger activityID;
+@property (nonatomic, strong) NSString* name;
+@property (nonatomic, strong) NSString* goal;
+@property (nonatomic, strong) NSDate* startDate;
+@property (nonatomic, strong) NSDate* endDate;
+@property (nonatomic, strong) NSString* description;
+@property (nonatomic) BOOL open;
+@property (nonatomic) NSInteger creatorID;
+
+@end
