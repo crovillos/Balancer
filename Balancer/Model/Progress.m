1de459bffacd7cca07c975dffc08799f5cc8dfe8
Dian Hartono <dhartono@uw.edu>
2013-05-09 22:22:13 -0700

    social and progress

---
+++ Balancer/Model/Progress.m
@@ -0,0 +1,13 @@
+//
+//  Progress.m
+//  Balancer
+//
+//  Created by Dian Hartono on 5/9/13.
+//  Copyright (c) 2013 Dian Hartono, Grace Jang, Chris Rovillos, Catriona Scott, Brian Yin. All rights reserved.
+//
+
+#import "Progress.h"
+
+@implementation Progress
+
+@end
---
+++ Balancer/Model/Social.m
@@ -0,0 +1,14 @@
+
+//
+//  Social.m
+//  Balancer
+//
+//  Created by Dian Hartono on 5/9/13.
+//  Copyright (c) 2013 Dian Hartono, Grace Jang, Chris Rovillos, Catriona Scott, Brian Yin. All rights reserved.
+//
+
+#import "Social.h"
+
+@implementation Social
+
+@end
