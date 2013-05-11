f3970f3b9b0d2f6a8d45264153b11eb149ea0262
Dian Hartono <dhartono@uw.edu>
2013-05-11 10:23:31 -0700

    added social view controller

---
+++ Balancer/SocialTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SocialTableViewController.h
+//  Balancer
+//
+//  Created by Dian Hartono on 5/11/13.
+//  Copyright (c) 2013 Dian Hartono, Grace Jang, Chris Rovillos, Catriona Scott, Brian Yin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SocialTableViewController : UITableViewController
+
+@end
