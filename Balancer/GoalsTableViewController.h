fe2ea59386c9c7a622b74dbc609788efd01925cf
Chris Rovillos <crovillos@icloud.com>
2013-05-11 10:22:45 -0700

    Reorganized stuff.

---
+++ Balancer/GoalsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  GoalsTableViewController.h
+//  Balancer
+//
+//  Created by Chris Rovillos on 5/11/13.
+//  Copyright (c) 2013 Dian Hartono, Grace Jang, Chris Rovillos, Catriona Scott, Brian Yin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GoalsTableViewController : UITableViewController
+
+@end
