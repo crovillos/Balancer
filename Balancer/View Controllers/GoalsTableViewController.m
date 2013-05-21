24f8bdef475527c64a8735cf77928d48e01227dc
Chris Rovillos <crovillos@icloud.com>
2013-05-21 03:37:32 -0700

    Goals expanding and contracting view works.

--- Balancer/View Controllers/GoalsTableViewController.m
+++ Balancer/View Controllers/GoalsTableViewController.m
@@ -65,9 +65,7 @@
 
 - (BOOL)tableView:(UITableView *)tableView canCollapseSection:(NSInteger)section
 {
-    if (section>0) return YES;
-    
-    return NO;
+    return YES;
 }
 
 
@@ -106,7 +104,7 @@
         {
             Goal *goal = (Goal*)self.goals[section];
             
-            return [goal.activities count]; // return rows when expanded
+            return [goal.activities count] + 1; // return rows when expanded
         }
         
         return 1; // only top row showing
@@ -114,7 +112,6 @@
     
     // Return the number of rows in the section.
     return 1;
-    //return [self.goals count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -158,16 +155,13 @@
             // all other rows
             Goal *goalAtSection = (Goal *)self.goals[indexPath.section];
             // have activity id row within goal section
-            Activity *activityAtRow = (Activity *)goalAtSection.activities[indexPath.row];
-            cell.textLabel.text = activityAtRow.description;
-            cell.accessoryView = nil;
-            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+            Activity *activityAtRow = (Activity *)goalAtSection.activities[indexPath.row - 1];
+            cell.textLabel.text = activityAtRow.name;
         }
     }
     else
     {
         cell = [tableView dequeueReusableCellWithIdentifier:StepCellIdentifier];
-        cell.accessoryView = nil;
         cell.textLabel.text = @"Normal Cell";
         
     }
