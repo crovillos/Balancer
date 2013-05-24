9108eedb72208452569c4ed0127f9251f5f5bf16
Chris Rovillos <crovillos@icloud.com>
2013-05-24 15:15:21 -0700

    Made buttons show and hide int he goals page.

--- Balancer/View Controllers/Goals Tab/GoalsTableViewController.m
+++ Balancer/View Controllers/Goals Tab/GoalsTableViewController.m
@@ -207,7 +207,7 @@
         if (!indexPath.row)
         {
             [self.tableView beginUpdates];
-            
+            UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
             // only first row toggles exapand/collapse
             [tableView deselectRowAtIndexPath:indexPath animated:YES];
             
@@ -222,15 +222,46 @@
                 rows = [self tableView:tableView numberOfRowsInSection:section];
                 [self.expandedSections removeIndex:section];
                 
-                // hide buttons
+                UIButton* addStepButton = (UIButton *)[cell viewWithTag:1];
+                UIButton* editGoalButton = (UIButton *)[cell viewWithTag:2];
+                UIButton* goalInfoButton = (UIButton *)[cell viewWithTag:3];
                 
+                // hide buttons
+                [UIView animateWithDuration:0.25
+                                      delay:0
+                                    options:nil
+                                 animations:^{
+                                     addStepButton.alpha = 0.0;
+                                     editGoalButton.alpha = 0.0;
+                                     goalInfoButton.alpha = 0.0;
+                                 }
+                                 completion:^(BOOL finished){
+                                     // if you want to do anything when animation is done, do it here
+                                 }
+                 ];
             }
             else
             {
                 [self.expandedSections addIndex:section];
                 rows = [self tableView:tableView numberOfRowsInSection:section];
                 
+                UIButton* addStepButton = (UIButton *)[cell viewWithTag:1];
+                UIButton* editGoalButton = (UIButton *)[cell viewWithTag:2];
+                UIButton* goalInfoButton = (UIButton *)[cell viewWithTag:3];
+                
                 // show buttons
+                [UIView animateWithDuration:0.25
+                                      delay:0
+                                    options:nil
+                                 animations:^{
+                                     addStepButton.alpha = 1.0;
+                                     editGoalButton.alpha = 1.0;
+                                     goalInfoButton.alpha = 1.0;
+                                 }
+                                 completion:^(BOOL finished){
+                                     // if you want to do anything when animation is done, do it here
+                                 }
+                 ];
                 
             }
             
@@ -240,7 +271,7 @@
                 [tmpArray addObject:tmpIndexPath];
             }
             
-            UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+            
             
             if (currentlyExpanded)
             {
