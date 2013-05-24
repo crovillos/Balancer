3585d5905b29d682fdb35853198fe0bad5202ac5
Chris Rovillos <crovillos@icloud.com>
2013-05-24 01:57:20 -0700

    Tab bar icons are now all white.

--- Balancer/View Controllers/Flat UI Superclasses/FlatTabBarController.m
+++ Balancer/View Controllers/Flat UI Superclasses/FlatTabBarController.m
@@ -20,6 +20,18 @@
     [self.tabBar configureFlatTabBarWithColor:[UIColor balancerPinkColor]
                                 selectedColor:[UIColor balancerDarkPinkColor]];
     self.tabBar.selectedImageTintColor = [UIColor whiteColor];
+    self.tabBar.shadowImage = [[UIImage alloc] init];
+    
+    UITabBarItem *tabBarItem1 = [self.tabBar.items objectAtIndex:0];
+    UITabBarItem *tabBarItem2 = [self.tabBar.items objectAtIndex:1];
+    UITabBarItem *tabBarItem3 = [self.tabBar.items objectAtIndex:2];
+    UITabBarItem *tabBarItem4 = [self.tabBar.items objectAtIndex:3];
+    
+    // overrides storyboard
+    [tabBarItem1 setFinishedSelectedImage:[UIImage imageNamed:@"social_icon.png"] withFinishedUnselectedImage:[UIImage imageNamed:@"social_icon.png"]];
+    [tabBarItem2 setFinishedSelectedImage:[UIImage imageNamed:@"goals_icon.png"] withFinishedUnselectedImage:[UIImage imageNamed:@"goals_icon.png"]];
+    [tabBarItem3 setFinishedSelectedImage:[UIImage imageNamed:@"today_logo.png"] withFinishedUnselectedImage:[UIImage imageNamed:@"today_logo.png"]];
+    [tabBarItem4 setFinishedSelectedImage:[UIImage imageNamed:@"progress_icon.png"] withFinishedUnselectedImage:[UIImage imageNamed:@"progress_icon.png"]];
 }
 
 @end
